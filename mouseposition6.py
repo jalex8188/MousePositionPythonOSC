6546815299602e486bc69158b12a90b25d2f9537
jalex8188 <jalex8188@gmail.com>
2018-01-25 07:17:42 +0000

    TypeError too many arguments
    Exception in thread Thread-1:
    Traceback (most recent call last):
      File "/usr/lib/python2.7/threading.py", line 801, in __bootstrap_inner
        self.run()
      File "mouseposition6.py", line 50, in run
        client.send( OSCMessage ("/MousePos/", [text]))
    TypeError: __init__() takes at most 2 arguments (3 given)

---
+++ mouseposition6.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+"""
+Corrected, the thread stops now.
+"""
+
+import sys
+import os
+from OSC import OSCClient, OSCMessage
+
+from time import sleep
+
+import gtk
+gtk.gdk.threads_init()
+
+import threading
+
+# uses the package python-xlib
+# from http://snipplr.com/view/19188/mouseposition-on-linux-via-xlib/
+# or: sudo apt-get install python-xlib
+from Xlib import display
+screen_root = display.Display().screen().root
+
+old_stdout = sys.stdout
+sys.stdout = open(os.devnull, 'w')
+
+client = OSCClient()
+client.connect( ("192.168.0.102", 7110) )
+
+
+def mousepos():
+    """mousepos() --> (x, y) get the mouse coordinates on the screen (linux, Xlib)."""
+    data = screen_root.query_pointer()._data
+    return data["root_x"], data["root_y"]
+
+
+class MouseThread(threading.Thread):
+    def __init__(self, parent, label):
+        threading.Thread.__init__(self)
+        self.label = label
+        self.killed = False
+
+    def run(self):
+        try:
+            while True:
+                if self.stopped():
+                    break
+                text = "{0}".format(mousepos())
+                self.label.set_text(text)
+                client.send( OSCMessage ("/MousePos/", [text]))
+                sleep(0.04)
+        except (KeyboardInterrupt, SystemExit):
+            sys.exit()
+
+    def kill(self):
+        self.killed = True
+
+    def stopped(self):
+        return self.killed
+
+
+
+class PyApp(gtk.Window):
+
+    def __init__(self):
+        super(PyApp, self).__init__()
+
+        self.set_title("Mouse coordinates 0.1")
+        self.set_size_request(250, 50)
+        self.set_position(gtk.WIN_POS_CENTER)
+        self.connect("destroy", self.quit)
+
+        label = gtk.Label()
+
+        self.mouseThread = MouseThread(self, label)
+        self.mouseThread.start()
+
+        fixed = gtk.Fixed()
+        fixed.put(label, 10, 10)
+
+        self.add(fixed)
+        self.show_all()
+
+    def quit(self, widget):
+        self.mouseThread.kill()
+        gtk.main_quit()
+
+
+if __name__ == '__main__':
+    app = PyApp()
+    gtk.main()
